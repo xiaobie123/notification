1a7ed52ae98c5a4f54f18eeab204f2ef5cf47a38
xiaobie123 <1018677364@qq.com>
2017-07-23 17:43:30 +0800

    加上必要注释

--- src/Notice.jsx
+++ src/Notice.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 export default class Notice extends Component {
   static propTypes = {
-    duration: PropTypes.number,
+    duration: PropTypes.number,//持续时间
     onClose: PropTypes.func,
     children: PropTypes.any,
   };
@@ -21,25 +21,25 @@ export default class Notice extends Component {
   };
 
   componentDidMount() {
-    if (this.props.duration) {
-      this.closeTimer = setTimeout(() => {
+    if (this.props.duration) {//设置持续时间后才可以执行定时器
+      this.closeTimer = setTimeout(() => {//什么时候吧自己给关闭
         this.close();
       }, this.props.duration * 1000);
     }
   }
 
-  componentWillUnmount() {
+  componentWillUnmount() {//组件卸载时关闭并释放定时器
     this.clearCloseTimer();
   }
 
-  clearCloseTimer = () => {
+  clearCloseTimer = () => {//去掉自动关闭并取消定时器
     if (this.closeTimer) {
       clearTimeout(this.closeTimer);
       this.closeTimer = null;
     }
   }
 
-  close = () => {
+  close = () => {//关闭本次notice
     this.clearCloseTimer();
     this.props.onClose();
   }
@@ -49,8 +49,8 @@ export default class Notice extends Component {
     const componentClass = `${props.prefixCls}-notice`;
     const className = {
       [`${componentClass}`]: 1,
-      [`${componentClass}-closable`]: props.closable,
-      [props.className]: !!props.className,
+      [`${componentClass}-closable`]: props.closable,//能不能手动关闭
+      [props.className]: !!props.className,//自定义class
     };
     return (
       <div className={classNames(className)} style={props.style}>
--- src/Notification.jsx
+++ src/Notification.jsx
@@ -9,32 +9,32 @@ import Notice from './Notice';
 let seed = 0;
 const now = Date.now();
 
-function getUuid() {
+function getUuid() {//生产唯一值
   return `rcNotification_${now}_${seed++}`;
 }
 
 class Notification extends Component {
-  static propTypes = {
+  static propTypes = {//需要拿到的属性类型
     prefixCls: PropTypes.string,
     transitionName: PropTypes.string,
     animation: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
     style: PropTypes.object,
   };
 
-  static defaultProps = {
-    prefixCls: 'rc-notification',
-    animation: 'fade',
-    style: {
+  static defaultProps = {//默认属性
+    prefixCls: 'rc-notification',// css class 前缀，    目的是方便修改样式（这个参数增加了样式的灵活性）
+    animation: 'fade',//动画
+    style: {//默认css样式
       top: 65,
       left: '50%',
     },
   };
 
-  state = {
+  state = {//组件状态
     notices: [],
   };
 
-  getTransitionName() {
+  getTransitionName() {//得到过度动画的名字       做一个优先级，   有transitionName就用，没有就用默认的animation
     const props = this.props;
     let transitionName = props.transitionName;
     if (!transitionName && props.animation) {
@@ -44,18 +44,18 @@ class Notification extends Component {
   }
 
   add = (notice) => {
-    const key = notice.key = notice.key || getUuid();
-    this.setState(previousState => {
+    const key = notice.key = notice.key || getUuid();//指定notice的id             这个key 其实最主要是对动画有用的
+    this.setState(previousState => {//这个     传入一个回调函数的形式  previousState表示前一个state
       const notices = previousState.notices;
-      if (!notices.filter(v => v.key === key).length) {
+      if (!notices.filter(v => v.key === key).length) {//包括这个key的对象没有存过的时候 才改变state
         return {
-          notices: notices.concat(notice),
+          notices: notices.concat(notice),//给数组增加一个值，即修改了状态
         };
       }
     });
   }
 
-  remove = (key) => {
+  remove = (key) => {//移除指定key的notice
     this.setState(previousState => {
       return {
         notices: previousState.notices.filter(notice => notice.key !== key),
@@ -66,6 +66,7 @@ class Notification extends Component {
   render() {
     const props = this.props;
     const noticeNodes = this.state.notices.map((notice) => {
+      //创建一个函数，它将从左到右调用所有函数的参数。
       const onClose = createChainedFunction(this.remove.bind(this, notice.key), notice.onClose);
       return (<Notice
         prefixCls={props.prefixCls}
@@ -87,25 +88,25 @@ class Notification extends Component {
   }
 }
 
-Notification.newInstance = function newNotificationInstance(properties) {
-  const { getContainer, ...props } = properties || {};
+Notification.newInstance = function newNotificationInstance(properties) {//为这个react组件添加一个类方法
+  const { getContainer, ...props } = properties || {};//拿到默认参数
   let div;
   if (getContainer) {
-    div = getContainer();
+    div = getContainer();//用户自己指定包裹层
   } else {
     div = document.createElement('div');
     document.body.appendChild(div);
   }
-  const notification = ReactDOM.render(<Notification {...props} />, div);
+  const notification = ReactDOM.render(<Notification {...props} />, div); //这样其实已经对组件渲染了   组件有状态时返回一个refs，无状态时返回一个null
   return {
     notice(noticeProps) {
       notification.add(noticeProps);
     },
-    removeNotice(key) {
+    removeNotice(key) {//根据key手动移除 某个notice
       notification.remove(key);
     },
     component: notification,
-    destroy() {
+    destroy() {//卸载它
       ReactDOM.unmountComponentAtNode(div);
       document.body.removeChild(div);
     },
